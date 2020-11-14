6887a276b5c1b0b8efa4bed0b0439bee5f9349a7
xsebastianx <sarnavat1@gmail.com>
2020-11-13 20:57:31 -0500

    Added escape character so that questions appear on seperate lines

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let option = readlineSync.question('Options: dig a hole, distract gaurd\n');
 function game() {
   if (option === 'dig a hole') {
     console.log("You've made it outside, but you are surrounded");
-    let option = readlineSync.question('Options: fight off all the gaurds, make a run for it');
+    let option = readlineSync.question('Options: fight off all the gaurds, make a run for it\n');
     if (option === "fight off all the gaurds") {
       console.log("Success, you're a baddass.  You escaped prison!")
     } else {
@@ -15,7 +15,7 @@ function game() {
     }
   } else if (option === 'distract gaurd') {
     console.log('You were able to sneak by the gaurd');
-    let option = readlineSync.question('Options: steal a gun from the gaurd, free the other inmates')
+    let option = readlineSync.question('Options: steal a gun from the gaurd, free the other inmates\n')
     if (option === 'free the other inmates') {
       console.log("You freed everyone.  They can't catch you all.  You escape and win!")
     } else {
